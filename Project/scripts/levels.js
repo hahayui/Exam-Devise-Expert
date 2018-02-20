02890e62f27cf7b1b78963d3064f48282199e4de
Elian Kurtev <lqnko_55@outlook.com>
2018-02-20 11:27:01 +0200

    First commit - Home page and Activity page are finished

---
+++ Project/scripts/levels.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+
+
+    $(window).on('scroll', function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+
+    } else {
+        $('nav').removeClass('black');
+    }
+})
+
+$("nav ul a[href^='#']").on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 70
+        },
+        3000,
+        function () {});
+});
+
+
+
+
+    //DATA - JSON
+
+    var section1 = document.querySelector('#moderate');
+    var section2=document.querySelector('#moderatelyActive');
+    var section3=document.querySelector('#adventuresome');
+
+    var requestURL = 'excursions.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function () {
+        var excursion = request.response;
+       //populateHeader(excursion);
+        showModerateActivity(excursion);
+        showModeratelyActiveActivity(excursion);
+        showAdventuresome(excursion);
+    }
+
+    function populateHeader(jsonObj) {
+        var myH1 = document.createElement('h1');
+        myH1.textContent = jsonObj['type'];
+        section1.appendChild(myH1);
+
+    }
+
+    function showModerateActivity(jsonObj) {
+        var moderateActivity = jsonObj['moderateActivity'];
+
+        for (i = 0; i <moderateActivity.length; i++) {
+            var myArticle = document.createElement('article');
+            var H1 = document.createElement('h1');
+            H1.className = "moderate_heading1";
+            var image1 = document.createElement('img');
+            var operatorCostDates = document.createElement('p');
+            operatorCostDates.className="oCD";
+            var pText = document.createElement('p');
+            pText.className="text";
+
+            H1.textContent = moderateActivity[i].name;
+            image1.setAttribute('src', moderateActivity[i].img1);
+            operatorCostDates.textContent=moderateActivity[i].operatorCostDates;
+            pText.textContent = moderateActivity[i].text1;
+
+
+            myArticle.appendChild(H1);
+            myArticle.appendChild(image1);
+            myArticle.appendChild(operatorCostDates);
+            myArticle.appendChild(pText);
+            section1.appendChild(myArticle);
+
+
+        }
+    }
+    function showModeratelyActiveActivity(jsonObj){
+        var moderatelyActiveActivity=jsonObj['moderatelyActiveActivity'];
+        for(j=0;j<moderatelyActiveActivity.length;j++){
+            var myArticle = document.createElement('article');
+            var H1 = document.createElement('h1');
+            H1.className = "moderate_heading1";
+
+            var image1 = document.createElement('img');
+
+            var operatorCostDates = document.createElement('p');
+            operatorCostDates.className="oCD";
+
+            var pText = document.createElement('p');
+            pText.className="text";
+
+            H1.textContent = moderatelyActiveActivity[j].name;
+            image1.setAttribute('src', moderatelyActiveActivity[j].img1);
+            operatorCostDates.textContent=moderatelyActiveActivity[j].operatorCostDates;
+            pText.textContent = moderatelyActiveActivity[j].text1;
+
+
+            myArticle.appendChild(H1);
+            myArticle.appendChild(image1);
+            myArticle.appendChild(operatorCostDates);
+            myArticle.appendChild(pText);
+            section2.appendChild(myArticle);
+
+        }
+    }
+    function showAdventuresome(jsonObj){
+        var adventuresomeActiviy=jsonObj['adventuresomeActivity'];
+        for(k=0;k<adventuresomeActiviy.length;k++){
+            var myArticle = document.createElement('article');
+            var H1 = document.createElement('h1');
+            H1.className = "moderate_heading1";
+
+            var image1 = document.createElement('img');
+
+            var operatorCostDates = document.createElement('p');
+            operatorCostDates.className="oCD";
+
+            var pText = document.createElement('p');
+            pText.className="text";
+
+            H1.textContent = adventuresomeActiviy[k].name;
+            image1.setAttribute('src', adventuresomeActiviy[k].img1);
+            operatorCostDates.textContent=adventuresomeActiviy[k].operatorCostDates;
+            pText.textContent = adventuresomeActiviy[k].text1;
+
+
+            myArticle.appendChild(H1);
+            myArticle.appendChild(image1);
+            myArticle.appendChild(operatorCostDates);
+            myArticle.appendChild(pText);
+            section3.appendChild(myArticle);
+
+        }
+    }
+});
---
+++ Project/scripts/main.js
@@ -0,0 +1,41 @@
+$(window).on('scroll', function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+
+    } else {
+        $('nav').removeClass('black');
+    }
+})
+/*
+var pictures = ['banner_1.png', 'banner_2.png'];
+var index = 0;
+var intId = setInterval(function(){
+            //index++;
+            if( index >= pictures.length){
+                index = 0;
+            }
+            $('.slideshow').css('background-image', 'url(../images/' + pictures[index] +')')
+            .css('background-image', 'url(../images/' + pictures[index] +')').fadeOut(1000)
+
+            index++;
+           }, 2000);*/
+
+setInterval(function () {
+
+    $('.slideshow > img:first')
+        .next()
+        .fadeOut(2000)
+        .delay(6000)
+        .fadeIn(2000)
+        .delay(6000)
+}, 2000);
+
+
+$("nav ul a[href^='#']").on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 70
+        },
+        1000,
+        function () {});
+});
